57699e605da66e457f53e1121d1c78136e452d72
CarlosWGama <carloswgama@gmail.com>
2024-02-21 22:19:25 -0300

    Removido fontes conflito no Iphone

--- App.tsx
+++ App.tsx
@@ -12,22 +12,22 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
 
-  const [fontsLoaded] = useFonts({ Spartan_400Regular, Spartan_700Bold });
-  const [updated, setUpdated] = useState(false)
+  // const [fontsLoaded] = useFonts({ Spartan_400Regular, Spartan_700Bold });
+  // const [updated, setUpdated] = useState(false)
   
-  useEffect(() => {
-    (async () => {    
-      try {
-        const update = await Updates.checkForUpdateAsync();
-        if (update.isAvailable) {
-          await Updates.fetchUpdateAsync();
-          await Updates.reloadAsync();
-        }
-      } catch (e) { }
-      setUpdated(true)
-    })()
-  }, [])
-  if (!fontsLoaded || !updated) return <LoadingScreen />;
+  // useEffect(() => {
+  //   (async () => {    
+  //     try {
+  //       const update = await Updates.checkForUpdateAsync();
+  //       if (update.isAvailable) {
+  //         await Updates.fetchUpdateAsync();
+  //         await Updates.reloadAsync();
+  //       }
+  //     } catch (e) { }
+  //     setUpdated(true)
+  //   })()
+  // }, [])
+  // if (!fontsLoaded || !updated) return <LoadingScreen />;
   
   return (
     <GestureHandlerRootView style={{flex:1}}>
